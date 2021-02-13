ac2a420ebdbcd2e9083a9ef898a87412bae62f5f
yanalasanthoshreddy <santhosh.201316@gmail.com>
2021-02-14 00:34:52 +0530

    added Slf4J dependency for log

--- src/main/java/com/emp/info/registry/WowEmployeeRegistryServiceApplication.java
+++ src/main/java/com/emp/info/registry/WowEmployeeRegistryServiceApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @EnableEurekaServer
 @SpringBootApplication
 public class WowEmployeeRegistryServiceApplication {
 
 	public static void main(String[] args) {
+		log.info("My Registry Service Application :: STARTED");
 		SpringApplication.run(WowEmployeeRegistryServiceApplication.class, args);
 	}
 
