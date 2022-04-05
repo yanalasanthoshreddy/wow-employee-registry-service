ef4184641cc3addefc76aee66a3799df33e1f1e4
SudhakarReddyJ <jsudhakarreddy46@gmail.com>
2022-04-05 10:49:44 +0530

    Added details doc

--- src/main/java/com/emp/info/registry/EmployeeRegistryApplication.java
+++ src/main/java/com/emp/info/registry/EmployeeRegistryApplication.java
@@ -10,13 +10,16 @@ import lombok.extern.slf4j.Slf4j;
  * Registry service boot applciation.
  * @author Sudhakar Reddy
  *
- *
  */
 @Slf4j
 @EnableEurekaServer
 @SpringBootApplication
 public class EmployeeRegistryApplication {
 
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		log.info("My Registry Service Application :: STARTED");
 		SpringApplication.run(EmployeeRegistryApplication.class, args);
